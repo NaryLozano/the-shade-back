3008254dd2bdef17aca133cd0b97a3f3aaabe3aa
nary <narylozano@gmail.com>
2023-05-25 11:59:27 +0200

    Start server

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
+import "./loadEnvironments.js";
 import createDebug from "debug";
+import { app } from "./server/index.js";
 
 const debug = createDebug("the-shade-of-it-all-api:root");
 
 const port = process.env.PORT ?? 4000;
 
-debug(`Testing debug ${port}`);
+app.listen(port, () => {
+  debug(`Listening on http://localhost:${port}`);
+});
