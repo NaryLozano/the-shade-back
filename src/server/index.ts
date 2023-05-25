ee8c3c5f3414d70ad20479279a777de0d8754c33
nary <narylozano@gmail.com>
2023-05-25 17:08:35 +0200

    Add ping Controller and instanced at index server

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,6 +2,11 @@ import "../loadEnvironments.js";
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import {
+  errorNotFound,
+  generalError,
+} from "./middlewares/errors/errorsMiddlewares.js";
+import pingController from "./controllers/ping/pingController.js";
 
 export const app = express();
 
@@ -14,3 +19,8 @@ const devOrigin = process.env.ALLOWED_ORIGIN_DEV;
 app.use(cors({ origin: devOrigin }));
 
 app.use(morgan("dev"));
+
+app.get("/", pingController);
+
+app.use(errorNotFound);
+app.use(generalError);
