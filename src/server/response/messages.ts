8bac26bfcb0bd36c45679b559deccbb29cb883d9
nary <narylozano@gmail.com>
2023-05-26 10:12:32 +0200

    Add response messages and statuscode variables

---
+++ src/server/response/messages.ts
@@ -0,0 +1,8 @@
+const messages = {
+  pong: "🏓 Pong",
+  internalServerErrorMessage: "Internal Server Error",
+  notFoundMessage: "Endpoint not Found",
+  invalidCredentialsMessage: "Invalid credentials",
+};
+
+export default messages;
---
+++ src/server/response/statuscodes.ts
@@ -0,0 +1,8 @@
+const statuscode = {
+  OK: 200,
+  internalServerError: 500,
+  notFound: 404,
+  unauthorized: 401,
+};
+
+export default statuscode;
