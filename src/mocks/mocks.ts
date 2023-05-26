599e20406e9205364dbab0373704f9e01549d2ea
nary <narylozano@gmail.com>
2023-05-26 15:01:03 +0200

    Add mocks for integration test of validation

--- src/mocks/mocks.ts
+++ src/mocks/mocks.ts
@@ -7,3 +7,15 @@ export const invalidUser = {
   password: "sashayaway",
   username: "rupert",
 };
+
+export const mockToken = "aTokenToRuleThemAll";
+
+export const userHashpassword = {
+  password: "$2y$10$gcdUrNchvTm9MdUdHw8YRuEAcOgLgej6/ScIFs.frXfyCaccqT28S",
+  username: "admin",
+};
+
+export const userCredentials = {
+  password: "admin",
+  username: "admin",
+};
