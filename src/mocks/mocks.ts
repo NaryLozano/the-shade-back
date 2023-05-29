90fbc07ee7589dba5b5dbff85e7661ee6358fec8
nary <narylozano@gmail.com>
2023-05-29 13:55:23 +0200

    Update mocks, queen schema, queen controller, path and auth imports

--- src/mocks/mocks.ts
+++ src/mocks/mocks.ts
@@ -11,7 +11,7 @@ export const invalidUser = {
 export const mockToken = "aTokenToRuleThemAll";
 
 export const mockTokenReal =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NDZmYTc3MTRkNDVmMDM1ZjFiNTBkYjEiLCJ1c2VyIjoiYWRtaW4iLCJpYXQiOjE2ODUxMzIxOTQsImV4cCI6MTY4NTIxODU5NH0.Cye9A9CoZ5NQdzZKSVOVJXC4F276-nFia55JIAanYCI";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NDZmYTc3MTRkNDVmMDM1ZjFiNTBkYjEiLCJ1c2VyIjoiYWRtaW4iLCJpYXQiOjE2ODUzMDQ5MDAsImV4cCI6MTY4NTM5MTMwMH0.4ehFZP3-I4Y1WvxSkQxtJarzFjZhoO-Dtd8Kj3FAR7s";
 
 export const userHashpassword = {
   password: "$2y$10$gcdUrNchvTm9MdUdHw8YRuEAcOgLgej6/ScIFs.frXfyCaccqT28S",
